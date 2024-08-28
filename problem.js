9fb96af932bf9e26a2b1f9ec8d04b841792468dd
Dev2139 <dev.patel.codinggita@gmail.com>
2024-08-28 21:51:38 +0530

    problems solved

--- problem.js
+++ problem.js
@@ -20,26 +20,68 @@ if (act_sum !=sum){
     console.log("no number is missing")
 }
 
+arr = [1, 3, 2, 4, 5, 8, 7];
 
-arr = [0,0,2,1,2,0,0]
-len = arr.length;
+for (let i = 0; i < arr.length - 1; i++) {
+  for (let j = 0; j < arr.length - i - 1; j++) {
+    if (arr[j] > arr[j + 1]) {
+      [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+    }
+  }
+}
+
+console.log(arr); 
+
+
+arr = [0,0,1,0,1,0,1]
 count0 = 0;
-for(i=0;i<len;i++){
+for(i=0;i<=arr.length;i++){
     if(arr[i]==0){
-        count0++
-    }console.log("0:"+count0)
-}
+        count0+=1;
+    }
+}console.log(count0)
 count1 = 0;
-for(i=0;i<len;i++){
-    if(arr[i]==1){
-        count1++
-    }console.log("1:"+count1)
-}
+for(j=0;j<=arr.length;j++){
+    if(arr[j]==1){
+        count1+=1
+    }
+}console.log(count1)
+arr1 = []
+for(k=1;k<=count0;k++){
+    arr1.push(0)
+}console.log(arr1)
+for(l=1;l<=count1;l++){
+    arr1.push(1)
+}console.log(arr1)
+
+
+
+arr = [0,0,2,1,2,0,0,1]
+count0 = 0;
+for(i=0;i<arr.length;i++){
+    if(arr[i]==0){
+        count0 += 1
+    }
+}console.log(count0);
+count1 = 0;
+for(j=0;j<arr.length;j++){
+    if(arr[j]==1){
+        count1+=1
+    }
+}console.log(count1)
 count2 = 0;
-for(i=0;i<len;i++){
-    if(arr[i]==2){
-        count2++
-    }console.log("2:"+count2)
-}
-arr1 = [count0+count1+count2]
-console.log(arr1)
+for(k=0;k<=arr.length;k++){
+    if(arr[k]==2){
+        count2+=1
+    }
+}console.log(count2)
+arr1 = []
+for(l=1;l<=count0;l++){
+    arr1.push(0)
+}console.log(arr1)
+for(m=1;m<=count1;m++){
+    arr1.push(1)
+}console.log(arr1)
+for(n=1;n<=count2;n++){
+    arr1.push(2)
+}console.log(arr1)
